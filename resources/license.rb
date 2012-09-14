d1996952bfe39586b8cb512e28483aecf37c4abe
Cameron Johnston <cameron@needle.com>
2012-09-14 09:38:26 -0600

    adding missing commas in license resource

--- resources/license.rb
+++ resources/license.rb
@@ -3,8 +3,8 @@ include Opscode::OpenSSL::Password
 actions :activate
 
 attribute :license,         :kind_of => String, :default => "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
-attribute :activation_code  :kind_of => String, :default => "123412341234"
-attribute :passphrase       :kind_of => String, :default => secure_password
-attribute :salt             :kind_of => [String,FalseClass], :default => false
-attribute :available        :kind_of => [TrueClass,FalseClass], :default => true
-attribute :data_bag         :kind_of => String, :default => 'zncrypt_licence_pool'
+attribute :activation_code, :kind_of => String, :default => "123412341234"
+attribute :passphrase,      :kind_of => String, :default => secure_password
+attribute :salt,            :kind_of => [String,FalseClass], :default => false
+attribute :available,       :kind_of => [TrueClass,FalseClass], :default => true
+attribute :data_bag,        :kind_of => String, :default => 'zncrypt_licence_pool'
