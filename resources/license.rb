0b83e1085debbe0e8f379205675eca0be0439d70
Cameron Johnston <cameron@needle.com>
2012-09-18 11:14:53 -0600

    removing now-unsed available attribute from resource

--- resources/license.rb
+++ resources/license.rb
@@ -4,5 +4,4 @@ attribute :license,         :kind_of => String, :default => String.new
 attribute :activation_code, :kind_of => String, :default => String.new
 attribute :passphrase,      :kind_of => String, :required => true
 attribute :salt,            :kind_of => String, :default => String.new
-attribute :available,       :kind_of => [TrueClass,FalseClass], :default => true
 attribute :data_bag,        :kind_of => String, :default => 'zncrypt_license_pool'
