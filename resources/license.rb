9eac809b1b59526d6a31260c3293137bb67d7d1e
Cameron Johnston <cameron@needle.com>
2012-09-18 10:28:26 -0600

    typo fixes in zncrypt_license resource

--- resources/license.rb
+++ resources/license.rb
@@ -1,7 +1,7 @@
 actions :activate
 
 attribute :license,         :kind_of => [String,FalseClass], :default => false
-attribute :activation_code, :kind_of => [String,FalseClas] :default => false
+attribute :activation_code, :kind_of => [String,FalseClass], :default => false
 attribute :passphrase,      :kind_of => String, :required => true
 attribute :salt,            :kind_of => [String,FalseClass], :default => false
 attribute :available,       :kind_of => [TrueClass,FalseClass], :default => true
