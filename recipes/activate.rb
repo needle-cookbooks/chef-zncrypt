0fc5f92aec46f4f87bf44c412ce267da3725156c
Cameron Johnston <cameron@needle.com>
2012-09-26 15:16:35 -0600

    trying to fix use of secure_password in the activation recipe

--- recipes/activate.rb
+++ recipes/activate.rb
@@ -1,7 +1,10 @@
 ::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
 
+rand_passphrase = secure_password
+rand_salt = secure_password
+
 zncrypt_license node['hostname'] do
-  passphrase secure_password
-  salt secure_password
+  passphrase rand_passphrase
+  salt rand_salt
   action :activate
 end
